6af234800e9b31c231280560c2981b91dfac1f7a
ludohome <ludovic@coquemert.fr>
2025-04-05 19:16:34 +0200

    Creation menu principale et son sous menu.

--- views/templates/main.php
+++ views/templates/main.php
@@ -20,14 +20,23 @@
 <body>
     <header>
         <nav>
-            <a href="index.php">Articles</a>
-            <a href="index.php?action=apropos">À propos</a>
+        <ul class="menu">
+          <li>  <a href="index.php">Articles</a> </li>
+          <li>  <a href="index.php?action=apropos">À propos</a> </li>
             <?php 
                 // Si on est connecté, on affiche le bouton de déconnexion, sinon, on affiche le bouton de connexion : 
                 if (isset($_SESSION['user'])) {
-                    echo '<a href="index.php?action=disconnectUser">Déconnexion</a>';
+                    echo ' <li class="menu-item"> <a href="index.php?action=disconnectUser">Connecté : Coquemert  Ludovic</a>';
+                    echo ' <ul class="submenu">';
+                        echo '<li> <a href="#">Liste des articles</a> </li>';
+                        echo '<li> <a href="#">Liste des commentaires</a> </li>';
+                        echo '<li> <a href="#">Liste des Utilisateurs</a> </li>';
+                    echo '</ul> </li>';
+                    echo '<li><a href="index.php?action=disconnectUser">Déconnexion</a></li>';
                 }
-                ?>
+            ?>
+
+        </ul>
         </nav>
         <h1>Emilie Forteroche</h1>
     </header>
